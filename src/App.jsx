4ba4252d03ff7fef354418ea0428989118201395
Tulsi Singh <tulsi456324@gmail.com>
2025-08-11 12:21:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,6 @@ import WeatherApp from "./WeatherApp";
 function App() {
   const appStyle = {
     backgroundImage: `url(${image})`,
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-    height: "100vh",
-    margin: 0,
-    padding: 0,
   };
   
   return (
